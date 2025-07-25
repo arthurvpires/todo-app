6dbbebf3a0a38deb51e162718ef8a92998e5b03d
Arthur <arthurvpires@live.com>
2025-07-25 16:04:03 -0300

    Remove logs

--- frontend/src/stores/auth.ts
+++ frontend/src/stores/auth.ts
@@ -9,13 +9,10 @@ export const useAuthStore = defineStore('auth', {
     setAuth(data) {
       this.user = data.user;
       this.accessToken = data.access_token;
-      console.log('Auth state after set:', { user: this.user, accessToken: this.accessToken });
-      console.log('LocalStorage after set:', localStorage.getItem('pinia/auth'));
     },
     logout() {
       this.user = null;
       this.accessToken = null;
-      console.log('LocalStorage after logout:', localStorage.getItem('pinia/auth'));
     },
   },
   persist: {
